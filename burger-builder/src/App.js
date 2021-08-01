1a91fd22469479df18e912fc0ddda5b92dda4a35
Ahnaf Sadik Inan <62875018+Hartz-I@users.noreply.github.com>
2021-08-02 03:42:37 +0600

    tried out css modules

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import classes from "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+
+import styles from "./style.module.css";
+
+class App extends Component {
+  render() {
+    const assignedClasses = [styles.red, styles.center];
+
+    console.log(assignedClasses.join(" "));
+    return <div className={assignedClasses.join(" ")}>Hello</div>;
+  }
 }
 
 export default App;
