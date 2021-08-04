0c214398916d680c7ebdfe06e3d4c108f4fcb8a2
Ahnaf Sadik Inan <62875018+Hartz-I@users.noreply.github.com>
2021-08-04 14:30:19 +0600

    added backdrop to modal

--- burger-builder/src/components/UI/Backdrop/Backdrop.js
+++ burger-builder/src/components/UI/Backdrop/Backdrop.js
@@ -1,1 +1,12 @@
 //used to remove something
+import React from "react";
+
+import classes from "./Backdrop.module.css";
+
+const backdrop = (props) => {
+  return props.show ? (
+    <div className={classes.Backdrop} onClick={props.clicked}></div>
+  ) : null;
+};
+
+export default backdrop;
--- burger-builder/src/components/UI/Modal/Modal.js
+++ burger-builder/src/components/UI/Modal/Modal.js
@@ -2,18 +2,23 @@
 import React from "react";
 
 import classes from "./Modal.module.css";
+import Aux from "../../../hoc/Auxillary";
+import Backdrop from "../Backdrop/Backdrop";
 
 const modal = (props) => {
   return (
-    <div
-      style={{
-        transform: props.show ? "scale(1)" : "scale(0)",
-        opacity: props.show ? "1" : "0",
-      }}
-      className={classes.Modal}
-    >
-      {props.children}
-    </div>
+    <Aux>
+      <Backdrop show={props.show} clicked={props.modalClosed} />
+      <div
+        style={{
+          transform: props.show ? "scale(1)" : "scale(0)",
+          opacity: props.show ? "1" : "0",
+        }}
+        className={classes.Modal}
+      >
+        {props.children}
+      </div>
+    </Aux>
   );
 };
 
